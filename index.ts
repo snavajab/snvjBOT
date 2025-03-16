3993d08502d065c716051c9765057b881c5dde64
Tommaso Morganti <tommaso.morganti01@gmail.com>
2025-03-16 03:55:45 +0100

    fix: fastify bound host

--- index.ts
+++ index.ts
@@ -200,6 +200,6 @@ api.get("/users", async () => {
   }
 })
 
-api.listen({ port: 3000 }, () => {
+api.listen({ port: 3000, host: "0.0.0.0" }, () => {
   console.log("API listening on port 3000")
 })
